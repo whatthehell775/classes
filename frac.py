51e6ddecf1218ad838375bdd0d4eaa15a0e9accb
tanayag <tanay_agrawal@hotmail.com>
2017-02-12 01:28:35 +0530

    edit

--- frac.py
+++ frac.py
@@ -1,37 +1,32 @@
 class fraction:
 
     def __init__(self,top,bottom):
-
-        self.nu=top
+		self.num=top
         self.den=bottom
 
     def show(self):
-
-        print self.nu,"/",self.den
+		print self.num,"/",self.den
 
     def __str__(self):
-        return str(self.nu)+'/'+str(self.den)
+        return str(self.num)+'/'+str(self.den)
 
     def float_d(self):
-
-        return float(self.nu)/float(self.den)
+		return float(self.num)/float(self.den)
 
     def int_d(self):
+		return int(self.num)/int(self.den)
 
-        return int(self.nu)/int(self.den)
-
-    def __add__(self,ofr):
-        
+    def __add__(self,other):
         from fractions import gcd
-        nnum = self.nu*ofr.den + ofr.nu*self.den
-        nden = ofr.den*self.den
+        nnum = self.num*other.den + other.num*self.den
+        nden = other.den*self.den
         g = gcd(nnum,nden)
         nnum = nnum/g
         nden = nden/g
         return fraction(nnum,nden)
 
     def __eq__(self,other):
-        f1 = self.nu*other.den
-        f2 = self.den*other.nu
+        f1 = self.num*other.den
+        f2 = self.den*other.num
         return f1==f2
         
