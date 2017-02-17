f1f229875ba7b0c378b94a7b7d8069aa3db04eb4
tanayag <tanay_agrawal@hotmail.com>
2017-02-18 02:58:59 +0530

    queue added

--- queue.py
+++ queue.py
@@ -1,19 +1,19 @@
 class Queue:
-    	def __init__(self):
-        	self.items = []
+	def __init__(self):
+		self.items = []
 	
 	def __str__(self):
 		return str(slef.items)	
 
-    	def isEmpty(self):
-        	return self.items == []
+	def isEmpty(self):
+		return self.items == []
 
-    	def enqueue(self, item):
-        	self.items.insert(0,item)
+	def enqueue(self, item):
+		self.items.insert(0,item)
 
-    	def dequeue(self):
-        	return self.items.pop()
+	def dequeue(self):
+		return self.items.pop()
 	
 	def size(self):
-        	return len(self.items)
+		return len(self.items)
 
