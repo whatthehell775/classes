3050a3a01b1be9e4d9d2fa2bb6ce4c46c3c9e051
tanayag <tanay_agrawal@hotmail.com>
2017-02-18 06:45:26 +0530

    dequeue.py

---
+++ dequeue.py
@@ -0,0 +1,17 @@
+class Dequeue():
+	def __init__(self):
+		self.items = []
+	def isEmpty(self):
+		return self.items==[]
+	def addFront(self,item):
+		self.items.append(item)
+	def addRear(self):
+		self.items.insert(0,item)
+	def removeFront(self,item):
+		self.items.pop()
+	def removeRear(self):
+		self.items.pop(0)
+	def size(self):
+		return len(self.items)
+	def __str__(self):
+		return str(self.items)
